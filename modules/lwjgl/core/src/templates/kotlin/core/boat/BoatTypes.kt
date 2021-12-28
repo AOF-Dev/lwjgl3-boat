f9fe2165525fd9f3e7054f4b6b4e6ec7201eb172
CosineMath <boat_jvm@qq.com>
2021-12-28 12:13:37 +0800

    platform(Boat): feat(core): Add DynamicLinkLoader and BoatTypes for Boat in templates

---
+++ modules/lwjgl/core/src/templates/kotlin/core/boat/BoatTypes.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package core.boat
+
+import org.lwjgl.generator.*
+
+val long = IntegerType("long", PrimitiveMapping.POINTER)
+val unsigned_long = IntegerType("unsigned long", PrimitiveMapping.POINTER, unsigned = true)
